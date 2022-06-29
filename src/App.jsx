88c43f7ee371abb01480a221b725978c50d047ff
Facuscar <facundo.scardillo@dracoder.com>
2022-06-29 15:25:28 -0300

    Add: first styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,39 @@
+import styled from '@emotion/styled';
+import cryptoImage from './img/crypto-image.png';
+
+const Container = styled.div`
+  max-width: 900px;
+  margin: 0 auto;
+  width: 90%;
+  @media( min-width: 992px ){
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    column-gap: 2rem;
+  }
+`
+
+const Image = styled.img`
+  max-width: 400px;
+  width: 80%;
+  margin: 100px auto 0 auto;
+  display: block;
+`
+
+const Heading = styled.h1`
+  font-family: 'Lato', sans-serif;
+  color: #FFF;
+  text-align: center;
+`
 
 function App() {
   return (
-    <h1>From App</h1>
+    <Container>
+      <Image src={cryptoImage} alt="image with different crypto logos"/>
+      <div>
+        <Heading>See real time market prices</Heading>
+      </div>
+      
+    </Container>
   )
 }
 
